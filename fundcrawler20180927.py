ce5a4a28eb8833d4335c7ee470be575bc44812c0
Jerry <31723422+Jerry1014@users.noreply.github.com>
2018-09-27 21:59:00 +0800

    Add files via upload

--- fundcrawler20180927.py
+++ fundcrawler20180927.py
@@ -3,6 +3,8 @@ import requests
 import time
 from fake_useragent import UserAgent
 import re
+import threading
+import os
 
 
 def get_fund_list():
@@ -32,72 +34,72 @@ def get_fund_list():
             f.write(key + ',' + value + ',\n')
 
 
-def get_past_performance():
-    # fund_list = pandas.read_csv('fund.csv', index_col=0, dtype={'基金代号': str, '基金名称': str})\
-    # .to_dict(orient='list')
-
-    fund_list = {'基金代号': [], '基金名称': []}
-    with open('fund_simple.csv', 'r') as f:
-        for i in f.readlines():
-            code, name, _ = i.split(',')
-            fund_list['基金代号'].append(code)
-            fund_list['基金名称'].append(name)
-
-    achievement = {'近1月': [], '近3月': [], '近6月': [], '近1年': [], '近3年': [], '成立来': []}
-    count = 0
-    error_count = 0
-    ua = UserAgent()
-    for i in fund_list['基金代号']:
-        count += 1
+def get_achievement(code, sign):
+    achievement = []
+    if sign > 0:
+        ua = UserAgent()
         header = {"User-Agent": ua.random}
-        page = requests.get('http://fund.eastmoney.com/'+i+'.html', headers=header)
+        page = requests.get('http://fund.eastmoney.com/' + code + '.html', headers=header)
         page.encoding = 'utf-8'
-        tem = re.search('(?:近1月|保本期收益)：.*?((\d+\.\d{2}%)|--).*?近1年：.*?((\d+\.\d{2}%)|--).*?近3月：.*?((\d+\.'
-                        '\d{2}%)|--).*?近3年：.*?((\d+\.\d{2}%)|--).*?近6月：.*?((\d+\.\d{2}%)|--).*?成立来：.*?((\d+\.'
-                        '\d{2}%)|--).*?基金类型', page.text)
+        tem = re.search('(?:近1月|保本期收益)：.*?((-?\d+\.\d{2}%)|--).*?近1年：.*?((-?\d+\.\d{2}%)|--).*?近3月：.*?((-?'
+                        '\d+\.\d{2}%)|--).*?近3年：.*?((-?\d+\.\d{2}%)|--).*?近6月：.*?((-?\d+\.\d{2}%)|--).*?成立来：'
+                        '.*?((-?\d+\.\d{2}%)|--).*?基金类型', page.text)
         try:
-            achievement['近1月'].append(tem.group(1))
-            achievement['近1年'].append(tem.group(3))
-            achievement['近3月'].append(tem.group(5))
-            achievement['近3年'].append(tem.group(7))
-            achievement['近6月'].append(tem.group(9))
-            achievement['成立来'].append(tem.group(11))
+            achievement.append(tem.group(1))
+            achievement.append(tem.group(5))
+            achievement.append(tem.group(9))
+            achievement.append(tem.group(3))
+            achievement.append(tem.group(7))
+            achievement.append(tem.group(11))
         except:
+            time.sleep(5)
+            achievement = get_achievement(code, sign-1)
+    else:
+        for i in range(6):
+            achievement.append('??')
+    return achievement
+
+
+def thread_get_past_performance(code, name, thread_lock):
+    tem = get_achievement(code, 3)
+    sign = 1
+    for i in tem:
+        if i[0] == '-':
+            if i[1] == '-':
+                continue
+            else:
+                sign = 0
+                break
+    tem.append(str(sign))
+    fund_all_msg = [code, name] + tem
+    thread_lock.acquire()
+    with open('fund_with_achievement.csv', 'a') as f:
+        for i in fund_all_msg:
+            f.write(i + ',')
+        f.write('\n')
+    thread_lock.release()
+
+
+def get_past_performance():
+    os.remove('fund_with_achievement.csv')
+    with open('fund_simple.csv', 'r') as f:
+        thread = []
+        thread_lock = threading.Lock()
+        count = 0
+
+        for i in f.readlines():
+            count += 1
             try:
-                page = requests.get('http://fund.eastmoney.com/' + i + '.html', headers=header)
-                page.encoding = 'utf-8'
-                tem = re.search(
-                    '(?:近1月|保本期收益)：.*?((\d+\.\d{2}%)|--).*?近1年：.*?((\d+\.\d{2}%)|--).*?近3月：.*?((\d+\.\d{2}'
-                    '%)|--).*?近3年：.*?((\d+\.\d{2}%)|--).*?近6月：.*?((\d+\.\d{2}%)|--).*?成立来：.*?((\d+\.\d{2}%)|-'
-                    '-).*?基金类型', page.text)
-                achievement['近1月'].append(tem.group(1))
-                achievement['近1年'].append(tem.group(3))
-                achievement['近3月'].append(tem.group(5))
-                achievement['近3年'].append(tem.group(7))
-                achievement['近6月'].append(tem.group(9))
-                achievement['成立来'].append(tem.group(11))
-            except:
-                error_count += 1
-                time.sleep(10)
-                print('------------------------')
-                achievement['近1月'].append('??')
-                achievement['近1年'].append('??')
-                achievement['近3月'].append('??')
-                achievement['近3年'].append('??')
-                achievement['近6月'].append('??')
-                achievement['成立来'].append('??')
-        print(count)
-
-    # pandas.DataFrame(fund_list, index=range(count)).to_csv('fund2.csv')
-
-    with open('fund_with_achievement.csv', 'w') as f:
-        for i in range(count):
-            f.write(fund_list['基金代号'][i] + ',' + fund_list['基金名称'][i] + ',' + achievement['近1月'][i] + ',' +
-                    achievement['近3月'][i] + ',' + achievement['近6月'][i] + ',' + achievement['近1年'][i] + ',' +
-                    achievement['近3年'][i] + ',' + achievement['成立来'][i] + ',\n')
-
-    print(error_count)
+                code, name, _ = i.split(',')
+            except ValueError:
+                break
+            t = threading.Thread(target=thread_get_past_performance, args=(code, name, thread_lock))
+            thread.append(t)
+            t.start()
+            print(count)
+            if count/10 == 0:
+                time.sleep(5)
 
 
-# get_fund_list()
+get_fund_list()
 get_past_performance()
