6bd50224a77961a652d34b8b6353cad7da59daa0
Jerry <757320383@qq.com>
2019-08-28 21:27:15 +0800

    将随机ua类的实现从脚本中分离

--- fundcrawler.py
+++ fundcrawler.py
@@ -8,60 +8,26 @@ from queue import Queue
 
 import requests
 from eprogress import LineProgress
-from fake_useragent import UserAgent, FakeUserAgentError
 
+# 载入随机UA模块，若无，则使用默认的chrome ua
+print('正在载入随机UA模块')
+try:
+    from FakeUA import FakeUA
+    ua = FakeUA()
+except ModuleNotFoundError:
+    print('脚本工作目录未发现随机UA模块FakeUA，使用默认的唯一的chrome UA（可能会影响爬取效果）')
 
-class MyUserAgent():
-    """
-    用于提供ua，单例模式，fake_ua能用则用，否则用自带的数据库，通过class.random获取随机ua
-    """
-
-    def __new__(cls, *args, **kwargs):
-        if not hasattr(cls, 'instance'):
-            cls.instance = super(MyUserAgent, cls).__new__(cls)
-        return cls.instance
-
-    def __init__(self):
-        self.fake_ua = None
-        self.some = [
-            'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36 OPR/26.0.1656.60',
-            'Opera/8.0 (Windows NT 5.1; U; en)',
-            'Mozilla/5.0 (Windows NT 5.1; U; en; rv:1.8.1) Gecko/20061208 Firefox/2.0.0 Opera 9.50',
-            'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; en) Opera 9.50',
-            'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:34.0) Gecko/20100101 Firefox/34.0',
-            'Mozilla/5.0 (X11; U; Linux x86_64; zh-CN; rv:1.9.2.10) Gecko/20100922 Ubuntu/10.10 (maverick) Firefox/3.6.10',
-            'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/534.57.2 (KHTML, like Gecko) Version/5.1.7 Safari/534.57.2',
-            'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36',
-            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
-            'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/534.16 (KHTML, like Gecko) Chrome/10.0.648.133 Safari/534.16',
-            'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.101 Safari/537.36',
-            'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko',
-            'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.11 TaoBrowser/2.0 Safari/536.11',
-            'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.71 Safari/537.1 LBBROWSER',
-            'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; LBBROWSER)',
-            'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E; LBBROWSER)"',
-            'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; QQBrowser/7.0.3698.400)',
-            'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E)',
-            'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.84 Safari/535.11 SE 2.X MetaSr 1.0',
-            'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SV1; QQDownload 732; .NET4.0C; .NET4.0E; SE 2.X MetaSr 1.0)',
-            'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Maxthon/4.4.3.4000 Chrome/30.0.1599.101 Safari/537.36',
-            'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.122 UBrowser/4.0.3214.0 Safari/537.36']
-
-    def __getattr__(self, item):
-        try:
-            if item == 'random':
-                if self.fake_ua is not None:
-                    return self.fake_ua.random
-                else:
-                    return random.choice(self.some)
-        except KeyError:
-            raise AttributeError(r"Object does'n has attribute '%s'" % item)
+    class TemporaryUA:
+        def __init__(self):
+            self.random = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) " \
+                          "Chrome/76.0.3809.100 Safari/537.36"
+    ua = TemporaryUA()
 
 
 def get_fund_list():
     """爬取简单的基金代码名称目录"""
     print('开始爬取。。。')
-    ua = MyUserAgent()
+
     header = {"User-Agent": ua.random}
     page = requests.get('http://fund.eastmoney.com/Data/Fund_JJJZ_Data.aspx?t=1&lx=1&letter=&gsid=&text=&sort=zdf,'
                         'desc&page=1,9999&feature=|&dt=1536654761529&atfc=&onlySale=0', headers=header)
@@ -94,7 +60,6 @@ def get_page(url, lock_thread_pool):
             proxy_all = {'http': proxy['ip']}  # 代理ip
         except IndexError:
             proxy_all = None
-        ua = MyUserAgent()
         header = {"User-Agent": ua.random}
 
         try:
