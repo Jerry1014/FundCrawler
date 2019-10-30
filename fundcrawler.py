6cd1321c66d4fbb4b494c21e5da7c33b5a641c5d
Jerry <757320383@qq.com>
2019-10-30 11:57:41 +0800

    移除使用代理的部分和基金筛选的部分

--- fundcrawler.py
+++ fundcrawler.py
@@ -100,35 +100,15 @@ def get_page(url, need_to_save_file_event):
     # 若出现错误，最多尝试remain_wrong_time次
     remain_wrong_time = 3
     while remain_wrong_time > 0:
-        # 随机选取代理ip和ua
-        try:
-            proxy = random.choice(proxies_http_list)  # 代理ip
-            proxy_all = {'http': proxy['ip']}  # 代理ip
-        except IndexError:
-            proxy_all = None
         header = {"User-Agent": ua.random}
 
         try:
-            page = requests.get(url, headers=header, proxies=proxy_all, timeout=(30, 70))
+            page = requests.get(url, headers=header, timeout=(30, 70))
             page.encoding = 'utf-8'
             return page.text
         except (requests.exceptions.ConnectionError, requests.exceptions.Timeout, requests.exceptions.HTTPError):
             remain_wrong_time -= 1
             need_to_save_file_event.set()
-
-            # 记录代理ip错误
-            try:
-                if proxy_all:
-                    if proxy['err_count'] == 5:
-                        proxies_http_list.remove(proxy)
-                    else:
-                        proxy['err_count'] += 1
-            except ValueError:
-                pass
-
-        except:
-            remain_wrong_time -= 1
-
     return None
 
 
@@ -404,94 +384,6 @@ def get_past_performance(all_fund_generator_or_list, first_crawling=True):
     return list(queue_give_up.get() for i in range(queue_give_up.qsize()))
 
 
-def get_time_from_str(time_str):
-    """
-    将文字描述的任职时间转为列表
-    :param time_str: 描述时间的str
-    :return [int(多少年),int(多少天)]
-    """
-    tem = re.search('(?:(\d)年又|)(\d{0,3})天', time_str).groups()
-    tem_return = list()
-    for i in tem:
-        if i:
-            tem_return.append(int(i))
-        else:
-            tem_return.append(0)
-    return tem_return
-
-
-def data_analysis(fund_with_achievement, choice_return_this, choice_time_this):
-    """
-    按传入的筛选策略，筛选出符合要求的基金
-    :param fund_with_achievement: 全部的基金信息文件名
-    :param choice_return_this: 要求的基金收益率
-    :param choice_time_this: 要求的任职时间
-    """
-    # 文件以a方式写入，先进行可能的文件清理
-    try:
-        os.remove(fund_choice_filename)
-    except FileNotFoundError:
-        pass
-
-    try:
-        with open(fund_choice_filename, 'w') as f:
-            if fund_with_achievement == all_index_fund_with_msg_filename:
-                f.write(header_index_fund)
-            else:
-                f.write(header_guaranteed_fund)
-
-        print('开始筛选基金。。。')
-        with open(fund_with_achievement, 'r') as f:
-            count = 0
-            all_lines = f.readlines()[1:]
-            len_of_lines = len(all_lines)
-            line_progress = LineProgress(title='基金筛选进度')
-
-            for i in all_lines:
-                # 逐条检查
-                count += 1
-                sign = 1
-
-                # 取基金信息，并按收益率和任职时间分类
-                _, _, one_month, three_month, six_month, one_year, three_year, from_st, _, this_tenure_time, \
-                this_return, all_tenure_time, _ = i.split(',')
-                return_all = [one_month, three_month, six_month, one_year, three_year, from_st, this_return]
-                time_all = [this_tenure_time, all_tenure_time]
-
-                # 信息未知或一月数据不存在（成立时间过短）的淘汰
-                if one_month == '??' or one_month == '--':
-                    continue
-
-                # 收益率部分的筛选
-                for j, k in zip(choice_return_this.values(), return_all):
-                    if k == '--':
-                        continue
-                    if float(k[:-1]) < j:
-                        sign = 0
-                        break
-
-                # 任职时间部分的筛选
-                if sign == 1:
-                    for j, k in zip(choice_time_this.values(), time_all):
-                        for l, m in zip(j, get_time_from_str(k)):
-                            if m > l:
-                                break
-                            elif m == l:
-                                continue
-                            else:
-                                sign = 0
-                                break
-
-                # 符合要求的保存进文件
-                if sign == 1:
-                    with open(fund_choice_filename, 'a') as f2:
-                        f2.write(i)
-                line_progress.update(count * 100 // len_of_lines)
-
-    except Exception as e:
-        print(e)
-
-
 if __name__ == '__main__':
     start_time = time.time()
     # 文件名设置
@@ -499,17 +391,9 @@ if __name__ == '__main__':
     all_index_fund_with_msg_filename = 'index_fund_with_achievement.csv'  # 指数/股票型基金完整信息
     all_guaranteed_fund_with_msg_filename = 'guaranteed_fund_with_achievement.csv'  # 保本型基金完整信息
     fund_need_handle_filename = 'fund_need_handle.csv'  # 保存需要重新爬取的基金
-    fund_choice_filename = 'fund_choice.csv'  # 保存筛选出的基金
 
     header_index_fund = '基金代码,基金名称,近1月收益,近3月收益,近6月收益,近1年收益,近3年收益,成立来收益,基金经理,本基金任职时间,本基金任职收益,累计任职时间,\n'
     header_guaranteed_fund = '基金代码,基金名称,近1月收益,近3月收益,近6月收益,近1年收益,近3年收益,保本期收益,基金经理,本基金任职时间,本基金任职收益,累计任职时间,\n'
-
-    # 打开保存在proxies_http.txt的http代理ip
-    proxies_http_list = list()
-    # with open('proxies_http.txt', 'r') as f:
-    #     for i in f.readlines()[1:]:
-    #         tem = {'ip': i[:-1], 'err_count': 0}
-    #         proxies_http_list.append(tem)
 
     # 基金总数 线程数
     sum_of_fund = 0
@@ -521,13 +405,5 @@ if __name__ == '__main__':
     if fail_fund_list:
         print('仍然还有爬取失败的基金如下')
         print(fail_fund_list)
-
-    # 对基金的筛选设置
-    choice_return = {'近1月收益': 9.34, '近3月收益': 25.31, '近6月收益': 9.46, '近1年收益': 21.21,
-                     '近3年收益': 25.00, '成立来收益/保本期收益': 0, '本基金任职收益': 20}
-    choice_time = {'本基金任职时间': [3, 0], '累计任职时间': [5, 0]}
-
-    # 筛选后的文件为fund_choice_filename的值，若还需要对保本型基金进来筛选，需要先备份
-    data_analysis(all_index_fund_with_msg_filename, choice_return, choice_time)
 
     print("\n爬取总用时", time.time() - start_time)
