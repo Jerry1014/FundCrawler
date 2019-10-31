f7f01be43e18707bdc9f6059182101f69510c112
Jerry <757320383@qq.com>
2019-10-31 15:50:52 +0800

    重构了基金信息解析的部分和对应测试

--- CrawlingFund.py
+++ CrawlingFund.py
@@ -11,6 +11,9 @@ from eprogress import LineProgress
 # 载入随机UA模块，若无，则使用默认的chrome ua
 print('正在载入随机UA模块')
 try:
+    # 下行为测试临时使用
+    raise ModuleNotFoundError
+
     from FakeUA import FakeUA
 
     ua = FakeUA()
@@ -30,66 +33,6 @@ except ModuleNotFoundError:
 
 class FundCrawlerException(Exception):
     pass
-
-
-class FundInfo:
-    """
-    基金信息，缺少基金经理部分
-    """
-
-    def __init__(self):
-        self.info = dict()
-
-    def get_header(self):
-        return ','.join(self.info.keys())
-
-    def get_info(self):
-        return ','.join(self.info.values())
-
-    def get_fund_kind(self):
-        try:
-            return self.info['fund_kind']
-        except KeyError:
-            return 'Unknown'
-
-
-class FundManagerInfo:
-    """
-    基金经理信息
-    """
-
-    def __init__(self):
-        self.info = dict()
-
-    def get_header(self):
-        return ','.join(self.info.keys())
-
-    def get_info(self):
-        return ','.join(self.info.values())
-
-
-class FundWithAllInfo:
-    """
-    完整的基金信息
-    """
-
-    def __init__(self, fund_info: FundInfo, fund_manager: FundManagerInfo):
-        self.fund_info = fund_info
-        self.fund_manager_info = fund_manager
-
-    def get_header(self):
-        """
-        获取基金信息第一行（索引）
-        :return: str 基金信息索引 逗号分割
-        """
-        return self.fund_info.get_header() + ',' + self.fund_manager_info.get_header()
-
-    def get_info(self):
-        """
-        获取该基金信息
-        :return: str 基金信息 逗号分割
-        """
-        return self.fund_info.get_info() + ',' + self.fund_manager_info.get_info()
 
 
 def get_fund_list():
@@ -113,164 +56,94 @@ def get_fund_list():
         yield f'%s,%s' % (i[1:7], i[10:-1])
 
 
-def get_page_context():
+class FundManager:
+    def __init__(self, position_time=0, term_return=0, this_fund_return=0):
+        self.position_time = position_time
+        self.term_return = term_return
+        self.this_fund_return = this_fund_return
+
+    def get_info(self):
+        return str(self.position_time) + ',' + str(self.term_return) + ',' + str(self.this_fund_return)
+
+
+class FundInfo:
     """
-    用于爬取页面 通过send url(str)爬取特定的网页，发送'kill'时结束
+    基金信息
+    """
+    def __init__(self):
+        self._fund_info = dict()
+        self._manager_info = FundManager()
+
+    def get_header(self):
+        return ','.join(self._fund_info.keys())
+
+    def get_info(self):
+        return ','.join(self._fund_info.values()) + ',' + self._manager_info.get_info()
+
+    def get_fund_kind(self):
+        try:
+            return self._fund_info['fund_kind']
+        except KeyError:
+            return 'Unknown'
+
+    def set_fund_info(self, key, value):
+        self._fund_info[key] = value
+
+    def __repr__(self):
+        return ' | '.join(str(key) + ',' + str(value) for key, value in self._fund_info.items())
+
+
+def get_page_context(url):
+    """
+    用于爬取页面 爬取特定的网页
+    :param url:要爬取的url
     :return: 迭代器 页面内容(str)
     """
-    result = ('init', None)
-    url = yield result
-    while url != 'kill':
-        header = {"User-Agent": ua.random}
-
-        try:
-            page = requests.get(url, headers=header, timeout=(30, 70))
-            page.encoding = 'utf-8'
-            result = ('success', page.text)
-        except (requests.exceptions.ConnectionError, requests.exceptions.Timeout, requests.exceptions.HTTPError):
-            result = ('error', None)
-        url = yield result
-
-
-def get_achievement(re_text, need_to_save_file_event):
-    """
-    用于爬取基金的收益率和基金经理的信息
-    :param need_to_save_file_event: 网络出错事件
-    :param re_text: 要清洗网页文本
-    :return 二元组（基金信息，基金种类），其中基金信息为list，近1月。3。6。近1年。3。（成立来收益率|保本期收益）（指数股票|保本），
-    基金经理/基金经理2.，任职时间，任职收益率，基金经理总任职时间/基金经理2总任职时间.
-    基金种类 1:指数/股票基金 2:保本型基金 3:封闭期又或者已终止的 4:未识别的
-    """
-    fund_kind = None
-    achievement = list()
-
-    # 基金的收益率
-    achievement_after_clean = re.search(
-        '近1月：.*?((-?\d+\.\d{2}%)|--).*?近1年：.*?((-?\d+\.\d{2}%)|--).*?近3月：.*?((-?\d+\.\d{2}%)|--).*?'
-        '近3年：.*?((-?\d+\.\d{2}%)|--).*?近6月：.*?((-?\d+\.\d{2}%)|--).*?成立来：.*?((-?\d+\.\d{2}%)|--).*?基金类型', re_text)
-    if not achievement_after_clean:
-        # 基金为保本型基金
-        achievement_after_clean = re.search('保本期收益.*?((-?\d+\.\d{2}%)|--).*?近6月：.*?((-?\d+\.\d{2}%)|--).*?'
-                                            '近1月：.*?((-?\d+\.\d{2}%)|--).*?近1年：.*?((-?\d+\.\d{2}%)|--).*?'
-                                            '近3月：.*?((-?\d+\.\d{2}%)|--).*?近3年：.*?((-?\d+\.\d{2}%)|--).*?基金类型', re_text)
-        if re.search('封闭期', re_text) or re.search('本基金已终止', re_text):
-            # 基金为有封闭期的固定收益基金或已终止的基金
-            fund_kind = 3
-            return list(), fund_kind
-        if achievement_after_clean:
-            fund_kind = 2
-        else:
-            fund_kind = 4
-            return list(), fund_kind
-    else:
-        fund_kind = 1
-
-    # 基金经理和个人信息链接
-    fund_manager_ifo = re.search('<td class="td02">(?:<a href="(.*?)">(.+?)</a>&nbsp;&nbsp;)(?:(?:<a href="(.*?)">(.+?)'
-                                 '</a>&nbsp;&nbsp;)|)', re_text)
-    # 基金经理在本基金的任职时间和收益率
-    fund_manager_detail = re.search(
-        '</td>  <td class="td03">(.+?)</td>  <td class="td04 bold (?:ui-color-(?:red|green)|)">'
-        '(-?\d+\.\d{2}%)</td></tr>', re_text)
-
-    # 对可能的多个基金经理分别记录以便后续爬取
-    manager_link_list = list()
-    manager_list = list()
-    i = 1
-    for j in fund_manager_ifo.groups():
-        if j:
-            if i % 2 == 0:
-                manager_list.append(j)
-            else:
-                manager_link_list.append(j)
-        i += 1
-    # 保存基金经理名字
-    manager = None
-    for i in manager_list:
-        if i != manager_list[0]:
-            manager += '/' + i
-        else:
-            manager = i
+    header = {"User-Agent": ua.random}
 
     try:
-        if fund_kind == 1:
-            # 保存基金收益率
-            achievement.append(achievement_after_clean.group(1))
-            achievement.append(achievement_after_clean.group(5))
-            achievement.append(achievement_after_clean.group(9))
-            achievement.append(achievement_after_clean.group(3))
-            achievement.append(achievement_after_clean.group(7))
-            achievement.append(achievement_after_clean.group(11))
-        else:
+        page = requests.get(url, headers=header, timeout=(30, 70))
+        page.encoding = 'utf-8'
+        result = ('success', page.text)
+    except (requests.exceptions.ConnectionError, requests.exceptions.Timeout, requests.exceptions.HTTPError):
+        result = ('error', None)
+    return result
+
+
+def parse_fund_info():
+    """
+    对基金信息界面进行解析 通过send(page_context)来获得解析
+    :return: 迭代器 FundInfo
+    """
+    index_header = ['近1月', '近1年', '近3月', '近3年', '近6月', '成立来']
+    guaranteed_header = ['保本期收益', '近6月', '近1月', '近1年', '近3月', '近3年']
+    result = None
+    page_context = yield result
+
+    while page_context != 'end':
+        result = FundInfo()
+
+        # 清洗基金收益率 此为指数/股票型的基金
+        achievement_re = re.search(r'：.*?((?:-?\d+\.\d{2}%)|--).*?'.join(index_header + ['基金类型']), page_context)
+        if not achievement_re:
             # 保本型基金
-            achievement.append(achievement_after_clean.group(5))
-            achievement.append(achievement_after_clean.group(9))
-            achievement.append(achievement_after_clean.group(3))
-            achievement.append(achievement_after_clean.group(7))
-            achievement.append(achievement_after_clean.group(11))
-            achievement.append(achievement_after_clean.group(1))
-
-        achievement.append(manager)
-        achievement.append(fund_manager_detail.group(1))
-        achievement.append(fund_manager_detail.group(2))
-    except AttributeError:
-        # 理论上应该是能有这个多个结果的，如果出错，1.正则表达式 2.特殊网页结构
-        fund_kind = 4
-
-    # 分别打开基金经理的个人信息页，保存他们的总任职时间
-    manager_link = None
-    for i in manager_link_list:
-        re_text = get_page_context(i, need_to_save_file_event)
-        if re_text:
-            fund_manager_appointment_time = re.search('<span>累计任职时间：</span>(.*?)<br />', re_text)
-            if i != manager_link_list[0]:
-                manager_link += '/' + fund_manager_appointment_time.group(1)
+            achievement_re = re.search(r'：.*?((-?\d+\.\d{2}%)|--).*?'.join(guaranteed_header + ['基金类型']), page_context)
+            if re.search('封闭期', page_context) or re.search('本基金已终止', page_context):
+                # 基金为有封闭期的固定收益基金或已终止的基金
+                result.set_fund_info('fund_kind', 'close')
+            if achievement_re:
+                result.set_fund_info('fund_kind', 'guaranteed')
+                for header, value in zip(guaranteed_header, achievement_re.groups()):
+                    result.set_fund_info(header, value)
             else:
-                manager_link = fund_manager_appointment_time.group(1)
+                result.set_fund_info('fund_kind', 'Unknown')
         else:
-            fund_kind = 4
-            break
-    achievement.append(manager_link)
+            result.set_fund_info('fund_kind', 'index')
+            for header, value in zip(guaranteed_header, achievement_re.groups()):
+                result.set_fund_info(header, value)
 
-    return achievement, fund_kind
-
-
-def thread_get_past_performance(code, name, queue_index_fund, queue_guaranteed_fund, queue_other_fund, queue_give_up,
-                                need_to_save_file_event):
-    """
-    爬取单个基金信息的线程
-    :param code: 基金代号
-    :param name: 基金名称
-    :param queue_index_fund: 保存爬取的指数/股票基金
-    :param queue_guaranteed_fund: 保存爬取的保本基金
-    :param queue_other_fund: 保存封闭期或者已终止的基金代码
-    :param queue_give_up: 保存放弃爬取的基金代码
-    :param need_to_save_file_event: 爬取时，发生网路错误事件
-    """
-    # 临时接受爬取函数返回的数据
-    tem = list()
-
-    re_text = get_page_context('http://fund.eastmoney.com/' + code + '.html', need_to_save_file_event)
-
-    if re_text is None:
-        # 重复出错3次后，放弃。
-        fund_kind = 4
-    else:
-        tem, fund_kind = get_achievement(re_text, need_to_save_file_event)
-    fund_all_msg = [code, name] + list(tem)
-
-    if fund_kind == 1:
-        # 指数型/股票型等基金
-        queue_index_fund.put(fund_all_msg)
-    elif fund_kind == 2:
-        # 保本型基金
-        queue_guaranteed_fund.put(fund_all_msg)
-    elif fund_kind == 3:
-        # 有封闭期的固定收益基金或已终止的基金
-        queue_other_fund.put(code + ',' + name + '\n')
-    else:
-        # 爬取失败
-        queue_give_up.put(code + ',' + name + '\n')
+        # todo 爬取基金经理的个人页面，获得总任职时间
+        page_context = yield result
 
 
 def get_past_performance(all_fund_generator_or_list, first_crawling=True):
@@ -281,6 +154,8 @@ def get_past_performance(all_fund_generator_or_list, first_crawling=True):
     :return 爬取失败的('基金代码,基金名称')(list)
     """
     maximum_of_thread = 1
+    header_index_fund = '基金代码,基金名称,近1月收益,近3月收益,近6月收益,近1年收益,近3年收益,成立来收益,基金经理,本基金任职时间,本基金任职收益,累计任职时间,\n'
+    header_guaranteed_fund = '基金代码,基金名称,近1月收益,近3月收益,近6月收益,近1年收益,近3年收益,保本期收益,基金经理,本基金任职时间,本基金任职收益,累计任职时间,\n'
     # 测试文件是否被占用，并写入列索引
     try:
         if first_crawling:
@@ -412,8 +287,6 @@ if __name__ == '__main__':
     all_guaranteed_fund_with_msg_filename = 'guaranteed_fund_with_achievement.csv'  # 保本型基金完整信息
     fund_need_handle_filename = 'fund_need_handle.csv'  # 保存需要重新爬取的基金
 
-    header_index_fund = '基金代码,基金名称,近1月收益,近3月收益,近6月收益,近1年收益,近3年收益,成立来收益,基金经理,本基金任职时间,本基金任职收益,累计任职时间,\n'
-    header_guaranteed_fund = '基金代码,基金名称,近1月收益,近3月收益,近6月收益,近1年收益,近3年收益,保本期收益,基金经理,本基金任职时间,本基金任职收益,累计任职时间,\n'
 
     # 基金总数 线程数
     sum_of_fund = 0
--- MonkeyTest.py
+++ MonkeyTest.py
@@ -1,16 +1,22 @@
 import unittest
 import CrawlingFund
+import time
 
 
 class MyTestCase(unittest.TestCase):
     def test_get_page_context(self):
         url = ['http://baidu.com']
 
-        my_test_iter = CrawlingFund.get_page_context()
-        my_test_iter.send(None)
         for i in url:
-            print(my_test_iter.send(i))
+            print(CrawlingFund.get_page_context(i))
 
+    def test_parse_fund_info(self):
+        url = ['http://fund.eastmoney.com/002939.html']
+
+        my_iter = CrawlingFund.parse_fund_info()
+        my_iter.send(None)
+        for i in url:
+            print(my_iter.send(CrawlingFund.get_page_context(i)[1]))
 
 if __name__ == '__main__':
     unittest.main()
