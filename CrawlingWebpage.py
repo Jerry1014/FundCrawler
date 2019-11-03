d58f2f5e98c89ec8e7a9c32a1f0e488a1106faf8
Jerry <757320383@qq.com>
2019-11-03 21:38:31 +0800

    简单完成线程池的实现

--- CrawlingWebpage.py
+++ CrawlingWebpage.py
@@ -2,6 +2,7 @@
 """
 负责接受url并爬取该网页
 """
+import threading
 from abc import ABC
 from multiprocessing import Process, Queue
 
@@ -24,7 +25,7 @@ class GetPage:
 
 class GetPageByWeb(GetPage, ABC):
     @classmethod
-    def get_page_context(cls, url) -> tuple:
+    def get_page_context(cls, url, *args) -> tuple:
         """
         用于爬取页面 爬取特定的网页
         :param url:要爬取的url
@@ -34,9 +35,9 @@ class GetPageByWeb(GetPage, ABC):
         try:
             page = requests.get(url, headers=header, timeout=(30, 70))
             page.encoding = 'utf-8'
-            result = ('success', page.text)
+            result = ('success', page.text, args)
         except (requests.exceptions.ConnectionError, requests.exceptions.Timeout, requests.exceptions.HTTPError):
-            result = ('error', None)
+            result = ('error', None, args)
         return result
 
 
@@ -47,12 +48,21 @@ class GetPageByWebWithAnotherProcessAndMultiThreading(GetPageByWeb, Process):
         self._result_queue = result_queue
         self._threading_pool = list()
         self._exit_when_task_queue_empty = False
+        self._max_threading_number = 1
 
     def add_task(self, task):
         self._task_queue.put(task)
 
     def get_result(self) -> Queue:
         return self._result_queue
+
+    def get_page_context(self, url, *args) -> tuple:
+        result = super().get_page_context(url, *args)
+        if result[0] == 'success':
+            self._max_threading_number += 1
+        else:
+            self._max_threading_number = self._max_threading_number << 1 if self._max_threading_number > 1 else 1
+        return result
 
     def run(self) -> None:
         while True:
@@ -62,5 +72,15 @@ class GetPageByWebWithAnotherProcessAndMultiThreading(GetPageByWeb, Process):
                 else:
                     continue
             else:
-                pass
-                # todo 线程数量管理
+                # 1 清除死去的线程
+                # 2 新建新的线程
+                for t in self._threading_pool:
+                    if not t.is_alive():
+                        self._threading_pool.remove(t)
+
+                while len(self._threading_pool) < self._max_threading_number:
+                    task = self._task_queue.get()
+                    t = threading.Thread(target=self.get_page_context,
+                                         args=('http://fund.eastmoney.com/' + task[0] + '.html', *task[1:]))
+                    self._threading_pool.append(t)
+                    t.start()
