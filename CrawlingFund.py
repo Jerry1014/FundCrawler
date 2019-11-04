8b1a506dd467d2841664caed44fd6776cca5a223
Jerry <757320383@qq.com>
2019-11-04 11:39:15 +0800

    简单完成了各个部分的组合，勉强能用，下次逐一修复bug

--- CrawlingFund.py
+++ CrawlingFund.py
@@ -1,21 +1,13 @@
 # -*- coding:UTF-8 -*-
-import random
 import re
-import threading
 import time
-from queue import Queue
+from collections import OrderedDict
+from multiprocessing import Queue, Event
 
 from eprogress import LineProgress
 
-
-class FundManager:
-    def __init__(self, position_time=0, term_return=0, this_fund_return=0):
-        self.position_time = position_time
-        self.term_return = term_return
-        self.this_fund_return = this_fund_return
-
-    def get_info(self):
-        return str(self.position_time) + ',' + str(self.term_return) + ',' + str(self.this_fund_return)
+from CrawlingWebpage import GetPageByWebWithAnotherProcessAndMultiThreading
+from ProvideTheListOfFund import GetFundListByWebForTest
 
 
 class FundInfo:
@@ -24,14 +16,13 @@ class FundInfo:
     """
 
     def __init__(self):
-        self._fund_info = dict()
-        self._manager_info = FundManager()
+        self._fund_info = OrderedDict()
 
     def get_header(self):
         return ','.join(self._fund_info.keys())
 
     def get_info(self):
-        return ','.join(self._fund_info.values()) + ',' + self._manager_info.get_info()
+        return ','.join(self._fund_info.values())
 
     def get_fund_kind(self):
         try:
@@ -46,13 +37,17 @@ class FundInfo:
         return ' | '.join(str(key) + ',' + str(value) for key, value in self._fund_info.items())
 
 
+# 这个是根据网页的html解析的顺序，若需要指定在爬取结果中的顺序，请修改index_of_header
+index_header = ['近1月', '近1年', '近3月', '近3年', '近6月', '成立来']
+guaranteed_header = ['保本期收益', '近6月', '近1月', '近1年', '近3月', '近3年']
+index_of_header = [0, 2, 4, 1, 3, 5]
+
+
 def parse_fund_info():
     """
     对基金信息界面进行解析 通过send(page_context)来获得解析
     :return: 迭代器 FundInfo
     """
-    index_header = ['近1月', '近1年', '近3月', '近3年', '近6月', '成立来']
-    guaranteed_header = ['保本期收益', '近6月', '近1月', '近1年', '近3月', '近3年']
     page_context = yield
 
     while True:
@@ -73,10 +68,9 @@ def parse_fund_info():
                 result.set_fund_info('fund_kind', 'Unknown')
         else:
             result.set_fund_info('fund_kind', 'index')
-            for header, value in zip(guaranteed_header, achievement_re.groups()):
+            for header, value in zip(index_header, achievement_re.groups()):
                 result.set_fund_info(header, value)
 
-        # todo 爬取基金经理的个人页面，获得总任职时间
         page_context = yield result
 
 
@@ -110,9 +104,8 @@ def get_past_performance(all_fund_generator_or_list, first_crawling=True):
     :param first_crawling: 是否是第一次爬取，这决定了是否会重新写保存文件（清空并写入列索引）
     :return 爬取失败的('基金代码,基金名称')(list)
     """
-    maximum_of_thread = 1
-    header_index_fund = '基金代码,基金名称,近1月收益,近3月收益,近6月收益,近1年收益,近3年收益,成立来收益,基金经理,本基金任职时间,本基金任职收益,累计任职时间,\n'
-    header_guaranteed_fund = '基金代码,基金名称,近1月收益,近3月收益,近6月收益,近1年收益,近3年收益,保本期收益,基金经理,本基金任职时间,本基金任职收益,累计任职时间,\n'
+    header_index_fund = ','.join([index_header[i] for i in index_of_header]) + '\n'
+    header_guaranteed_fund = ','.join([guaranteed_header[i] for i in index_of_header]) + '\n'
     # 测试文件是否被占用，并写入列索引
     try:
         if first_crawling:
@@ -132,53 +125,40 @@ def get_past_performance(all_fund_generator_or_list, first_crawling=True):
 
     # 进度条
     line_progress = LineProgress(title='爬取进度')
-
-    # 线程集合 爬取结果队列
-    thread = list()
+    # 爬取结果队列
+    input_queue = Queue()
     result_queue = Queue()
+    finish_sign = Event()
+    GetPageByWebWithAnotherProcessAndMultiThreading(input_queue, result_queue, finish_sign).start()
 
-    need_to_save_file_event = threading.Event()
-
+    # todo 爬取出错时，结束爬虫进程
     while True:
+        # todo 进度条
         i = next(all_fund_generator_or_list)
         try:
             code, name = i.split(',')
             name = name[:-1]
         except ValueError:
             continue
+        except:
+            # bug 未填 'StopIteration' object has no attribute 'split'
+            break
 
-        # 多线程爬取
-        t = threading.Thread(target=get_page_context,
-                             args=('http://fund.eastmoney.com/' + code + '.html', result_queue))
-        thread.append(t)
-        t.setName(code + ',' + name)
-        t.start()
-        for t in thread:
-            if not t.is_alive():
-                thread.remove(t)
+        input_queue.put(('http://fund.eastmoney.com/' + code + '.html',))
 
-        if len(thread) > maximum_of_thread:
-            time.sleep(random.random())
-            while len(thread) > maximum_of_thread // 2:
-                for t in thread:
-                    if not t.is_alive():
-                        thread.remove(t)
+    # todo 暂时不做基金经理这部分
+    finish_sign.set()
+    while finish_sign.is_set(): pass
 
-        line_progress.update(result_queue.qsize() * 100 // sum_of_fund)
-
-    # 等待所有线程执行完毕
-    while len(thread) > 0:
-        line_progress.update((sum_of_fund - len(thread)) * 100 // sum_of_fund)
-        time.sleep(random.random())
-        for t in thread:
-            if not t.is_alive():
-                thread.remove(t)
+    web_page_parse = parse_fund_info()
+    next(web_page_parse)
+    while not result_queue.empty():
+        print(web_page_parse.send(result_queue.get()[1]))
 
     line_progress.update(99)
-    need_to_save_file_event.set()
+    # todo 保存文件
     line_progress.update(100)
-    print('\n基金信息爬取完成，其中处于封闭期或已终止的基金有' + str(queue_other_fund.qsize()) + '个，爬取失败的有' + str(queue_give_up.qsize()) + '个')
-    return list(queue_give_up.get() for i in range(queue_give_up.qsize()))
+    # todo 对第一次爬取失败的基金的处理
 
 
 if __name__ == '__main__':
@@ -189,15 +169,7 @@ if __name__ == '__main__':
     all_guaranteed_fund_with_msg_filename = 'guaranteed_fund_with_achievement.csv'  # 保本型基金完整信息
     fund_need_handle_filename = 'fund_need_handle.csv'  # 保存需要重新爬取的基金
 
-    # 基金总数 线程数
-    sum_of_fund = 0
-
-    # 获取基金过往数据 重新获取第一次失败的数据
-    fail_fund_list = get_past_performance(get_fund_list())
-    print('\n对第一次爬取失败的基金进行重新爬取\n')
-    fail_fund_list = get_past_performance(fail_fund_list, False)
-    if fail_fund_list:
-        print('仍然还有爬取失败的基金如下')
-        print(fail_fund_list)
+    # just for test
+    get_past_performance(GetFundListByWebForTest().get_fund_list())
 
     print("\n爬取总用时", time.time() - start_time)
