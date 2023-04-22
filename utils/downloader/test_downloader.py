7b5a561d486c8cd8c7f510f2071a96da5b79e86d
Jerry <xiangming.huang@qq.com>
2023-04-22 15:59:44 +0800

    feature 移动downloader位置

--- utils/downloader/test_downloader.py
+++ utils/downloader/test_downloader.py
@@ -6,19 +6,17 @@ from utils.downloader.async_downloader import BaseRequest
 from utils.downloader.impl.http_request_downloader import AsyncHttpRequestDownloader, Request
 
 
-class TestUniqueKey(BaseRequest.UniqueKey):
-    def __init__(self):
-        pass
-
-
 class TestDownloader(TestCase):
+    class TestUniqueKey(BaseRequest.UniqueKey):
+        def __init__(self):
+            pass
 
     def test_request_downloader(self):
         url = "https://www.baidu.com"
         downloader = AsyncHttpRequestDownloader()
 
         for i in range(10):
-            downloader.summit(Request(TestUniqueKey(), url))
+            downloader.summit(Request(TestDownloader.TestUniqueKey(), url))
 
         downloader.shutdown()
         while downloader.has_next_result():
