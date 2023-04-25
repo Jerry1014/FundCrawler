25ec47bf9b9639d543d1e9901581c87590fe29e7
Jerry <xiangming.huang@qq.com>
2023-04-25 17:53:18 +0800

    feature 下载模块优化

--- utils/downloader/async_downloader.py
+++ utils/downloader/async_downloader.py
@@ -6,9 +6,13 @@ from typing import Optional, TypeVar, NoReturn
 
 
 class BaseRequest:
+    """
+    请求
+    """
+
     class UniqueKey(ABC):
         """
-        下载唯一键，用于确认某个请求是谁发起的
+        下载唯一键, 用于确认某个请求是谁发起的, 业务实现
         """
         pass
 
@@ -21,17 +25,24 @@ R = TypeVar('R', bound=BaseRequest)
 
 
 class BaseResponse:
+    """
+    返回
+    """
+
     def __init__(self, request: R, response):
         self.request = request
         self.response = response
 
 
 class AsyncHttpDownloader(ABC):
+    """
+    下载工具
+    """
+
     @abstractmethod
     def summit(self, request: BaseRequest) -> NoReturn:
         """
         提交http下载请求
-        :param request: req
         """
         return NotImplemented
 
@@ -39,15 +50,15 @@ class AsyncHttpDownloader(ABC):
     def get_result(self) -> Optional[BaseResponse]:
         """
         获取下载结果
-        :return 结果，None代表暂无结果
+        :return 结果, None代表暂无结果
         """
         return NotImplemented
 
     @abstractmethod
     def has_next_result(self) -> bool:
         """
-        http爬取结果已经全部获取完
-        在 shutdown后，当返回false时，可以认为所有的结果均已得到处理
+        当前的爬取结果已经全部获取完
+        在 shutdown后调用，当返回false时，可以认为所有的结果均已被处理
         :return: false，当前结果已经处理完
         """
         return NotImplemented
@@ -55,6 +66,7 @@ class AsyncHttpDownloader(ABC):
     @abstractmethod
     def shutdown(self) -> NoReturn:
         """
-        爬取结束，可以关闭（不再接受summit，但仍然可以获取正在爬取的结果）
+        爬取结束，可以关闭（不再接受summit，但仍然可以获取正在/已经爬取完的结果）
+        不代表结果已经处理完, 需要结合 has_next_result判断
         """
         return NotImplemented
