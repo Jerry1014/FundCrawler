a486a30c27c4867f32d7fb05bee2cffc40a7598b
Jerry <xiangming.huang@qq.com>
2023-04-28 16:59:09 +0800

    feature 将爬取速率控制单独提取成独立类

--- utils/downloader/impl/http_request_downloader.py
+++ utils/downloader/impl/http_request_downloader.py
@@ -89,9 +89,16 @@ class AsyncHttpRequestDownloader(AsyncHttpDownloader):
 
         def __init__(self, request_queue: Queue, result_queue: Queue, exit_sign: synchronize.Event):
             super().__init__()
+            # 和父进程之间的通信
             self._request_queue = request_queue
             self._result_queue = result_queue
             self._exit_sign = exit_sign
+
+            # 爬取速率控制
+            self._rate_control = AsyncHttpRequestDownloader.RateControl()
+
+            logging.basicConfig(filename='downloader.text', encoding='utf-8', level=logging.INFO, filemode='w',
+                                format='%(asctime)s %(message)s')
 
         @staticmethod
         def get_page(request: Request) -> Response:
@@ -112,19 +119,9 @@ class AsyncHttpRequestDownloader(AsyncHttpDownloader):
             """
             爬取主流程
             """
-            logging.basicConfig(filename='downloader.text', encoding='utf-8', level=logging.INFO, filemode='w',
-                                format='%(asctime)s %(message)s')
-
             executor = ThreadPoolExecutor()
             future_list: list[Future] = []
             need_retry_task_list: list[Request] = list()
-
-            circle_count = 5
-            success_count_ring = [0] * circle_count
-            fail_count_ring = [0] * circle_count
-            number_of_iterations = 0
-            request_once_handle_max_num = 1.0
-            last_fail_request_once_handle_max_num = request_once_handle_max_num
 
             while True:
                 # 爬取结束
@@ -135,58 +132,84 @@ class AsyncHttpRequestDownloader(AsyncHttpDownloader):
                     self._exit_sign.clear()
                     break
 
-                # 处理爬取结果
-                new_future_list: list[Future] = list()
+                # 获取已完成的task
+                need_handle_result_list: list[Response] = list()
                 for future in future_list:
-                    if not future.done():
-                        new_future_list.append(future)
+                    if future.done():
+                        result: Response = future.result()
+                        need_handle_result_list.append(result)
+                        future_list.remove(future)
                         continue
 
-                    result: Response = future.result()
+                # 处理爬取结果
+                for result in need_handle_result_list:
                     if result.state == Response.State.FALSE and result.request.retry_time > 0:
                         # 失败重试
                         result.request.retry_time -= 1
                         need_retry_task_list.append(result.request)
                         continue
                     self._result_queue.put(result)
-                old_future_list = future_list
-                future_list = new_future_list
 
                 # 爬取速率控制
-                new_success_count = 0
-                new_fail_count = 0
-                for future in old_future_list:
-                    result: Response = future.result()
-                    if result.state == Response.State.FALSE:
-                        new_fail_count += 1
-                    else:
-                        new_success_count += 1
-                success_count_ring[number_of_iterations % circle_count] = new_success_count
-                fail_count_ring[number_of_iterations % circle_count] = new_fail_count
-                total = sum(success_count_ring) + sum(fail_count_ring)
-                fail_rate = (sum(fail_count_ring) / total) if total != 0 else 0.0
-
-                if fail_rate > 0.0:
-                    last_fail_request_once_handle_max_num = request_once_handle_max_num
-                    request_once_handle_max_num = max(1, request_once_handle_max_num / 2)
-                else:
-                    request_once_handle_max_num = max(int(last_fail_request_once_handle_max_num / 2),
-                                                      request_once_handle_max_num + 0.01)
-                logging.info(f"当前爬取失败率{fail_rate} 最大任务数{request_once_handle_max_num}")
+                success_count = sum(
+                    [1 if result.state == Response.State.SUCCESS else 0 for result in need_handle_result_list])
+                number_of_concurrent_tasks = self._rate_control \
+                    .get_cur_number_of_concurrent_tasks(success_count, len(need_handle_result_list) - success_count)
 
                 # 处理爬取请求
-                tem_request_once_handle_max_num = int(request_once_handle_max_num)
                 while (not self._request_queue.empty() or len(need_retry_task_list) > 0) \
-                        and tem_request_once_handle_max_num > 0:
+                        and number_of_concurrent_tasks > 0:
+                    # 优先处理需要重试的任务
                     request = need_retry_task_list.pop() if len(need_retry_task_list) > 0 else self._request_queue.get()
                     future_list.append(executor.submit(self.get_page, request))
-                    tem_request_once_handle_max_num -= 1
+                    number_of_concurrent_tasks -= 1
 
                 # 休眠主线程，避免循环占用过多的cpu时间
                 sleep(0.1)
-                number_of_iterations += 1
 
             # 确保数据都写入后，再退出主线程
             # OS pipes are not infinitely long, so the process which queues data could be blocked in the OS during the
             # put() operation until some other process uses get() to retrieve data from the queue
             self._result_queue.join_thread()
+
+    class RateControl:
+        """
+        速率控制
+        根据当前请求的失败率 决策当前的爬取速率
+        """
+
+        def __init__(self):
+            # 记录环，记录最近circle_count次的成功失败次数
+            self._circle_count = 5
+            self._success_count_ring = [0] * self._circle_count
+            self._fail_count_ring = [0] * self._circle_count
+            self._number_of_iterations = 0
+
+            # 当前认为的最适合并发任务数 float
+            self._cur_number_of_concurrent_tasks = 1.0
+            # 上一次失败上升时的并发任务数
+            self._last_cur_number_of_concurrent_tasks = self._cur_number_of_concurrent_tasks
+
+            self._rising_step = 0.01
+
+        def get_cur_number_of_concurrent_tasks(self, success_count, fail_count) -> int:
+            """
+            根据当前的成功失败任务个数，决策当前最合适的并发任务数
+            """
+            self._success_count_ring[self._number_of_iterations % self._circle_count] = success_count
+            self._fail_count_ring[self._number_of_iterations % self._circle_count] = fail_count
+
+            total = sum(self._success_count_ring) + sum(self._fail_count_ring)
+            fail_rate = (sum(self._fail_count_ring) / total) if total != 0 else 0.0
+
+            #
+            if fail_rate > 0.0:
+                self._last_cur_number_of_concurrent_tasks = self._cur_number_of_concurrent_tasks
+                self._cur_number_of_concurrent_tasks = max(1, self._cur_number_of_concurrent_tasks / 2)
+            else:
+                self._cur_number_of_concurrent_tasks = max(self._last_cur_number_of_concurrent_tasks / 2,
+                                                           self._cur_number_of_concurrent_tasks + self._rising_step)
+            logging.info(f"当前爬取失败率{fail_rate} 最大任务数{self._cur_number_of_concurrent_tasks}")
+
+            self._number_of_iterations += 1
+            return int(self._cur_number_of_concurrent_tasks)
